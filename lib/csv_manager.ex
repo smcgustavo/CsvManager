db8c7cc386f2564d231477fe72548472150ba95d
Gustavo Santos <gustavosmc@ufrrj.br>
2024-01-10 01:51:28 -0300

    csv_info

--- lib/csv_manager.ex
+++ lib/csv_manager.ex
@@ -17,24 +17,38 @@ defmodule CsvManager do
       {:error, reason} -> raise "The #{filename} couldn't be read by: #{reason}"
     end
   end
-
+  
+  
   def parse_data(%CsvManager.Data{file: file, sep: sep} = data) do
     rows = String.split(file, "\n") 
+    [first | _tail] = rows 
     parsed_data 
       = 
       for row <- rows do
         String.split(row, sep)
       end
-    %CsvManager.Data{data | parsed_data: parsed_data}
+    
+    %CsvManager.Data{
+      data | 
+      parsed_data: parsed_data, 
+      number_of_rows: length(rows),
+      columns: first,
+      number_of_columns: length(String.split(first, sep))
+    }
+  end
+  
+  def csv_info(%CsvManager.Data{file_name: name, number_of_rows: n_rows, number_of_columns: n_cols, columns: cols}) do
+    IO.puts "Name: #{name}\nNumber of Rows: #{n_rows}\nNumber of Columns: #{n_cols}\nColumns: #{cols}"
   end
 
-  def read(filename, sep) do
+  def read_csv(filename, sep) do
     read_file(filename, sep)
     |> parse_data
   end
 
-  def read(filename) do
+  def read_csv(filename) do
     read_file(filename, ",")
     |> parse_data
   end
+
 end
--- lib/data.ex
+++ lib/data.ex
@@ -2,8 +2,9 @@ defmodule CsvManager.Data do
   defstruct [
     file: nil,
     file_name: nil,
-    number_of_rows: 0,
-    number_of_columns: 0,
+    columns: nil,
+    number_of_rows: nil,
+    number_of_columns: nil,
     sep: ",",
     parsed_data: nil
   ] 
