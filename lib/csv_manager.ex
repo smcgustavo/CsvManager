2e03180be605c238988d25a9d7b6a83e97e96ff4
Gustavo Santos <gustavosmc@ufrrj.br>
2024-01-14 03:58:46 -0300

    get row and column working

--- lib/csv_manager.ex
+++ lib/csv_manager.ex
@@ -38,17 +38,16 @@ defmodule CsvManager do
       number_of_columns: length(String.split(first, sep))
     }
   end
+
   @doc """
     Display csv info.
     Inputs `%CsvManager.Data`(the data structure)
   
   """
-
   def csv_info(%CsvManager.Data{file_name: name, number_of_rows: n_rows, number_of_columns: n_cols, columns: cols}) do
-    "Name: #{name}\nNumber of Rows: #{n_rows}\nNumber of Columns: #{n_cols}\nColumns: #{cols}"
+    IO.puts("Name: #{name}\nNumber of Rows: #{n_rows}\nNumber of Columns: #{n_cols}\nColumns: #{cols}")
   end
   
-
   @doc """
     Main function to read a csv file.
     Inputs `filename`(the file name) and `sep`(character to split the rows)
@@ -60,24 +59,57 @@ defmodule CsvManager do
   end
   
   @doc """
-    Main function to read a csv file.
-    Input `filename`(the file name) the module estipulates a "," for spliting rows
-    
+    Function to create map with columns-> column_index
   """
-  
   def map_columns(%CsvManager.Data{columns: columns, sep: sep} = data) do
     columns_map = 
       columns
       |> String.split(sep)
       |> Enum.with_index
       |> Map.new
-    %CsvManager.Data{ data | columns: columns_map}
+    %CsvManager.Data{ data | columns_map: columns_map}
+  end
+  
+  @doc """
+    
+    Returns a specific row from the data
+    Inputs `data` and `number` (number of the row)      
+  
+  """
+  def get_row(%CsvManager.Data{parsed_data: parsed_data, number_of_rows: number_of_rows}, number) do
+    case number > 0 && number < number_of_rows do
+      true -> elem(parsed_data, number)
+      false -> "Row number out of range."
+    end
+  end
+  
+  @doc """
+    
+    Acess all the values from a column
+    Inputs `data` and `column_name`
+
+  """
+  def get_column(%CsvManager.Data{parsed_data: parsed_data, columns_map: columns_map}, column_name) do
+    index = Map.fetch(columns_map, column_name)
+    if index == :error do 
+      raise "This columns couldn't be acessed."
+    end
+    {_status, value} = index
+    
+    for row <- Tuple.to_list(parsed_data) do
+      elem(row, value)
+    end
   end
 
+  @doc """
+    Main function to read a csv file.
+    Input `filename`(the file name) the module estipulates a "," for spliting rows
+    
+  """
   def read_csv(filename) do
     read_file(filename, ",")
     |> parse_data
     |> map_columns
   end
-
+  
 end
--- lib/data.ex
+++ lib/data.ex
@@ -3,6 +3,7 @@ defmodule CsvManager.Data do
     file: nil,
     file_name: nil,
     columns: nil,
+    columns_map: nil,
     number_of_rows: nil,
     number_of_columns: nil,
     sep: ",",
